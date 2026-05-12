cab9f76cc1e08720a421793f8cb6fad17d28387b
polcarrillo <polcarrillo@iesmontsia.org>
2026-05-12 17:21:19 +0200

    solventacio de logout

--- includes/logout.php
+++ includes/logout.php
@@ -6,8 +6,8 @@
  * @package GestioMaterial
  */
 
-require_once __DIR__ . '/includes/auth.php';
-require_once __DIR__ . '/includes/config.php';
+require_once __DIR__ . '/auth.php';
+require_once __DIR__ . '/config.php';
 
 iniciarSessio();
 tancarSessio();
